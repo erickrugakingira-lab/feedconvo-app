4cbbe20b21b25abf694e2c5bbeaafc80d67c1953
erickrugakingira-lab <erickrugakingira@gmail.com>
2026-03-23 12:41:54 +0300

    Update feedconvo.py

--- feedconvo.py
+++ feedconvo.py
@@ -83,7 +83,7 @@ st.markdown(f"""
     }}
 /* 2. The Content Box - Made more opaque (0.98) and added Blur */
     .main {{ 
-        background-color: rgba(255, 255, 255, 0.98); /* Almost solid white for focus */
+        background-color: rgba(255, 255, 255, 0.2); /* Almost solid white for focus */
         backdrop-filter: blur(1px); /* Blurs the photo BEHIND the white box */
         padding: 40px; 
         border-radius: 20px;
