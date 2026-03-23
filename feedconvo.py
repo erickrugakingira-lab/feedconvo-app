9b0e9f5fa52ccf13a99e375ec7449429c4386a3b
erickrugakingira-lab <erickrugakingira@gmail.com>
2026-03-23 14:25:42 +0300

    Update feedconvo.py

--- feedconvo.py
+++ feedconvo.py
@@ -8,7 +8,6 @@ import os
 st.set_page_config(page_title="FeedConvo Poultry Pro", layout="wide", page_icon="🐔")
 
 # --- 2. THE DATABASES (Global Scope) ---
-ING_DATABASE = {
    ING_DATABASE = {
     "Maize": {
         "img": "maize.jpg", "prot": 9.0, "en": 3350, "price_per_kg": 850,
@@ -49,7 +48,6 @@ ING_DATABASE = {
             "❌ **Red Flag:** Uwepo wa nyuzi za magunia / Presence of gunny bag fibers"
         ]
     }
-}
 
 STANDARDS = {
     "Starter (Wk 1-2)": 22.0, 
